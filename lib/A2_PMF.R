f63340d85a9751bd8df8ca78d1f2e1c4020772e7
LUYUE CHEN <47196797+clycrystal@users.noreply.github.com>
2019-11-16 21:44:25 -0500

    Update A2_PMF.R

--- lib/A2_PMF.R
+++ lib/A2_PMF.R
@@ -1,7 +1,6 @@
-#Define a function to calculate RMSE
 RMSE <- function(rating, est_rating){
   sqr_err <- function(obs){
-    sqr_error <- (obs[3] - est_rating[as.character(obs[2]), as.character(obs[1])])^2
+    sqr_error <- (as.numeric(obs[3]) - est_rating[as.character(obs[2]), as.character(obs[1])])^2
     return(sqr_error)
   }
   return(sqrt(mean(apply(rating, 1, sqr_err))))  
@@ -31,13 +30,11 @@ gradesc <- function(f = 10,
     colnames(grad_p) <- as.character(1:U)
     grad_q <- matrix(0, ncol=I, nrow=f)
     colnames(grad_q) <- levels(as.factor(data$movieId))
-    RMSE <- matrix(0, nrow=epochs, ncol=2)
-    est_rating <- vector(mode="list")
     
     for (i in 1:nrow(train)){
       user <- as.character(train[i,1])
       movie <- as.character(train[i,2])
-      error <- train[i,3]-t(p[,user])%*%q[,movie]
+      error <- as.numeric(train[i,3])-t(p[,user])%*%q[,movie]
       grad_p[,user] <- grad_p[,user]+error*q[,movie]
       grad_q[,movie] <- grad_q[,movie]+error*p[,user]
     }
@@ -45,29 +42,26 @@ gradesc <- function(f = 10,
     for(i in 1:U){
       grad_p[,as.character(i)] <- grad_p[,as.character(i)]-lambda_p*p[,as.character(i)]
     }
-    for(i in 1:levels(as.factor(data$movieId))){
+    for(i in levels(as.factor(data$movieId))){
       grad_q[,i] <- grad_q[,i]-lambda_q*q[,i]
     }
     
     p <- p + lrate*grad_p
     q <- q + lrate*grad_q
-  
-    # automatically change learning rate to avoid diverge
     
+  # aotomatically change learning rate to avoid diverge
     
-    #print the values of training and testing RMSE
+    #print the values of training and testing RMSE every 10 epochs
     if (l %% 10 == 0){
+      est <- t(q) %*% p
+    rownames(est) <- levels(as.factor(data$movieId))
+    train_RMSE_cur <- RMSE(train,est)
+    test_RMSE_cur <- RMSE(test, est)
+    train_RMSE <- c(train_RMSE, train_RMSE_cur)
+    test_RMSE <- c(test_RMSE, test_RMSE_cur)
       cat("epochs:", l, "\t")
-      est_rating <- t(q) %*% p
-      rownames(est_rating) <- levels(as.factor(data$movieId))
-      
-      train_RMSE_cur <- RMSE(train, est_rating)
       cat("training RMSE:", train_RMSE_cur, "\t")
-      train_RMSE <- c(train_RMSE, train_RMSE_cur)
-      
-      test_RMSE_cur <- RMSE(test, est_rating)
       cat("test RMSE:",test_RMSE_cur, "\n")
-      test_RMSE <- c(test_RMSE, test_RMSE_cur)
     }
   }
   
